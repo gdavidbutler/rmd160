952bd39314fa1d9784b35840fab89ca57eeace3d
G. David Butler <gdavidbutler@users.noreply.github.com>
2024-06-11 15:21:32 +0000

    add void to empty declarations

--- rmd160.h
+++ rmd160.h
@@ -19,7 +19,7 @@
  */
 
 typedef struct rmd160 rmd160_t;
-unsigned int rmd160tsize();
+unsigned int rmd160tsize(void);
 void rmd160init(rmd160_t *);
 void rmd160update(rmd160_t *, const unsigned char *, unsigned int);
 void rmd160final(rmd160_t *, unsigned char *); /* 20 unsigned char (160 bits) */
